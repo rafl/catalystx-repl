6ec2649bca33696af91cbba09f85847f672f3304
Florian Ragwitz <rafl@debian.org>
2009-02-26 03:20:52 +0100

    Update docs.

--- lib/CatalystX/REPL.pm
+++ lib/CatalystX/REPL.pm
@@ -22,8 +22,8 @@ use namespace::clean -except => 'meta';
 
 =head1 DESCRIPTION
 
-This role automatically sets up Carp::REPL for your application is in debug
-mode and the C<CATALYST_REPL> or C<MYAPP_REPL> environment variabeles are set.
+This role automatically sets up Carp::REPL after starting your application, if
+the C<CATALYST_REPL> or C<MYAPP_REPL> environment variabeles are set.
 
 =cut
 
