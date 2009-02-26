396b05456ae08995a4e378dbbb68ce918ac6ef84
Florian Ragwitz <rafl@debian.org>
2009-02-26 03:06:29 +0100

    Add basic docs.

--- lib/CatalystX/REPL.pm
+++ lib/CatalystX/REPL.pm
@@ -7,6 +7,26 @@ use Catalyst::Utils;
 
 use namespace::clean -except => 'meta';
 
+=head1 SYNOPSIS
+
+    package MyApp;
+
+    use Moose;
+
+    extends 'Catalyst';
+    with 'CatalystX::REPL';
+
+    __PACKAGE__->setup(qw/-Debug/);
+
+    1;
+
+=head1 DESCRIPTION
+
+This role automatically sets up Carp::REPL for your application is in debug
+mode and the C<CATALYST_REPL> or C<MYAPP_REPL> environment variabeles are set.
+
+=cut
+
 after setup_finalize => sub {
     my ($self) = @_;
     $SIG{__DIE__} = \&Carp::REPL::repl
