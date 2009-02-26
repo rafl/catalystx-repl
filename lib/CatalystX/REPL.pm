f3619b9caa94181bd9b49754f10f595047d4c7a7
Florian Ragwitz <rafl@debian.org>
2009-02-26 03:03:53 +0100

    Handele MYAPP_REPL in addition to CATALYST_REPL.

--- lib/CatalystX/REPL.pm
+++ lib/CatalystX/REPL.pm
@@ -3,13 +3,14 @@ package CatalystX::REPL;
 
 use Moose::Role;
 use Carp::REPL ();
+use Catalyst::Utils;
 
 use namespace::clean -except => 'meta';
 
 after setup_finalize => sub {
     my ($self) = @_;
     $SIG{__DIE__} = \&Carp::REPL::repl
-        if $self->debug && $ENV{CATALYST_REPL};
+        if $self->debug && Catalyst::Utils::env_value($self, 'repl');
 };
 
 1;
