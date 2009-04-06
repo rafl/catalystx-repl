d5dde462fac0c1dffd060d3dd3780dea30529d86
Ash Berlin <ash_github@firemirror.com>
2009-04-06 22:30:08 +0100

    Doc improvments (largley just examples)

--- lib/CatalystX/REPL.pm
+++ lib/CatalystX/REPL.pm
@@ -13,6 +13,7 @@ use namespace::clean -except => 'meta';
 
     use Moose;
 
+    # Requires Catalyst 5.8 series
     extends 'Catalyst';
     with 'CatalystX::REPL';
 
@@ -22,13 +23,42 @@ use namespace::clean -except => 'meta';
 
 =head1 DESCRIPTION
 
-Using L<Carp::REPL|Carp::REPL> with a Catalyst application is hard. That's because of all the
-internal exceptions that are being thrown and caught by Catalyst during
-application startup. You'd have to manually skip over all of those.
+Using L<Carp::REPL|Carp::REPL> with a Catalyst application is hard. That's
+because of all the internal exceptions that are being thrown and caught by
+Catalyst during application startup. You'd have to manually skip over all of
+those.
 
 This role works around that by automatically setting up Carp::REPL after
 starting your application, if the C<CATALYST_REPL> or C<MYAPP_REPL> environment
-variables are set.
+variables are set:
+
+ MYAPP_REPL=1 ./script/myapp_server.pl
+ # Hit an action
+ ...
+
+ 42 at lib/MyApp/Controller/Foo.pm line 8.
+
+ # instead of exiting, you get a REPL!
+ Trace begun at lib/MyApp/Controller/Foo.pm line 8
+ MyApp::Controller::Foo::bar('MyApp::Controller::Foo=HASH(0xc9fe20)', 'MyApp=HASH(0xcea6a4)') called at ...
+ ... # Many more lines of stack trace
+
+ $ $c
+ MyApp=HASH(0xcea6ec)
+ $ $c->req->uri          
+ http://localhost/foo/bar
+ $ 
+
+Carp::REPL uses L<Devel::REPL> for the shell, so direct any questions how how
+to use or customize the repl at that module.
+ 
+=head1 SEE ALSO
+
+L<Carp::REPL>
+
+L<Devel::REPL>
+
+L<Devel::REPL::Plugin>
 
 =cut
 
