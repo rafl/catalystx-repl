8b63c8fedbba064657375d8480f56b3dc7965c9b
Florian Ragwitz <rafl@debian.org>
2009-08-11 05:57:56 +0200

    Make sure to really not load the repl.rc.
    Also don't set CATALYST_DEBUG anymore. Only CATALYST_REPL is needed for a while
    now.

--- t/request.pl
+++ t/request.pl
@@ -1,13 +1,11 @@
 use strict;
 use warnings;
 
-BEGIN { @ENV{qw/CATALYST_DEBUG CATALYST_REPL/} = (1, 1); }
+BEGIN { @ENV{qw/CATALYST_REPL/} = 'noprofile'; }
 
 use FindBin;
 use lib "$FindBin::Bin/lib";
 
 use Catalyst::Test 'TestApp';
-
-$Carp::REPL::noprofile = 1;
 
 request($ARGV[0]);
